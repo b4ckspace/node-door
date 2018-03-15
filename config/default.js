2a4359ad60718ae2e5402c901c139a33aafb2b28
schinken <schinken@bamberg.ccc.de>
2018-03-15 21:51:00 +0100

    Add ldap parameters

--- config/default.js
+++ config/default.js
@@ -2,5 +2,13 @@ module.exports = {
 
     firmata: {
         port: '/dev/ttyUSB0'
+    },
+
+    ldap: {
+        uri: 'ldaps://127.0.0.1',
+        port: 636,
+        dn: '',
+        password: '',
+        ca: ''
     }
 };
